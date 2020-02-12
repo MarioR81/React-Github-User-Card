d006bd5abe14713f0785b56fda195a5504d23f89
apple <apple@apples-MacBook-Pro-2.local>
2020-02-11 17:53:31 -0800

    periodic save

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import './App.css';
+import axios from 'axios';
+import Card from './components/Card';
+
 
 class App extends React.Component {
   state = {
+    user: [],
     followers: []
   };
-  
+
+  componentDidMount() {
+    axios
+    .get('https://api.github.com/users/marior81')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        user: res.data
+      })
+    })
+    .catch(err => console.log(err));
+  }
+
+
   render(){
   return (
     <div className="App">
+      <h2>Git Hub Highlights</h2>
+        <p>{this.state.user.login}</p>
+        
+        <img src={this.state.user.avatar_url}/>
+        <p>{this.state.user.bio}</p>
+        <p>{this.state.user.login}</p>
+        <p>{this.state.user.login}</p>
+        <p>{this.state.user.login}</p>
         <p>
-          App
+           {/* <Card /> */}
         </p>
     </div>
   );
---
+++ my-app/src/components/Card.js
@@ -0,0 +1,34 @@
+// import React from 'react';
+// import axios from 'axios';
+
+
+
+// class Card extends React.Component {
+//   state = {
+   
+//   };
+
+//   componentDidMount() {
+//     axios
+//     .get('https://api.github.com/users/marior81')
+//     .then(res => {
+//       console.log(res.data)
+//       this.setState({
+//         user: res.data
+//       })
+//     })
+//     .catch(err => console.log(err));
+//   }
+
+//   render(){
+//   return (
+//     <div className="App">
+//         <p>
+//           Card
+//         </p>
+//     </div>
+//   );
+//   }
+// }
+
+// export default Card;
